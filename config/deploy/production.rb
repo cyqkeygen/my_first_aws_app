9d186655de544654a53a6a5fd993a6446183b17a
Yuquan Chen <cyqsdy@gmail.com>
2016-03-21 01:49:56 +0800

    change deploy user ec2-user to root

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,20 +5,20 @@ set :repository, "https://github.com/cyqkeygen/my_first_aws_app.git"
 set :deploy_to, "/home/ec2-user/production"
 set :scm, :git
 set :branch, "master"
-set :user, "ec2-user"
+set :user, "root"
 set :rails_env, "production"      # If the environment differs from the stage name
 set :deploy_via, :copy
 set :keep_releases, 5
-default_run_options[:pty] = true
+# default_run_options[:pty] = true
 
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '52.87.226.22', user: 'c2-user', roles: %w{app db web}, my_property: :my_value
-server '52.87.226.22', user: 'c2-user', roles: %w{app web}, other_property: :other_value
-server '52.87.226.22', user: 'c2-user', roles: %w{db}
+server '52.87.226.22', user: 'root', roles: %w{app db web}, my_property: :my_value
+server '52.87.226.22', user: 'root', roles: %w{app web}, other_property: :other_value
+server '52.87.226.22', user: 'root', roles: %w{db}
 
 
 
